1aa384f053b3baaed18837a28eb68c7120473992
Mathew Warger <mwarger@gmail.com>
2016-12-02 11:07:18 -0600

    added input and stuff

--- day1.js
+++ day1.js
@@ -6,15 +6,15 @@ var x = 0;
 var y = 0;
 direction = "North";
 var coords = [];
-function Coordinate(x, y){
-     this.x = x;
-     this.y = y;
+function Coordinate(x, y) {
+    this.x = x;
+    this.y = y;
 }
 
 //Math.abs(number);
 
-function getDirection(turn, currentDirection){
-    switch(currentDirection){
+function getDirection(turn, currentDirection) {
+    switch (currentDirection) {
         case 'North':
             return (turn === 'L') ? 'West' : 'East';
         case 'South':
@@ -28,16 +28,16 @@ function getDirection(turn, currentDirection){
 
 function addXAmount(currentDirection, amount, x) {
     var num = 0;
-    
+
     if (currentDirection === 'East') {
         num = x + amount;
-        for (var i = 0; i < amount; i++ ) {
+        for (var i = 0; i < amount; i++) {
             coords.push(new Coordinate(x + i, y));
         }
         return num;
     } else if (currentDirection === 'West') {
         num = x - amount;
-        for (var i = 0; i < amount; i++ ) {
+        for (var i = 0; i < amount; i++) {
             coords.push(new Coordinate(x - i, y));
         }
         return num;
@@ -45,16 +45,16 @@ function addXAmount(currentDirection, amount, x) {
 }
 
 function addYAmount(currentDirection, amount, y) {
-     var num = 0;
-     if (currentDirection === 'North') {
-        num =  y + amount;
-        for (var i = 0; i < amount; i++ ) {
+    var num = 0;
+    if (currentDirection === 'North') {
+        num = y + amount;
+        for (var i = 0; i < amount; i++) {
             coords.push(new Coordinate(x, y + i));
         }
         return num;
     } else if (currentDirection === 'South') {
         num = y - amount;
-        for (var i = 0; i < amount; i++ ) {
+        for (var i = 0; i < amount; i++) {
             coords.push(new Coordinate(x, y - i));
         }
         return num;
@@ -62,26 +62,25 @@ function addYAmount(currentDirection, amount, y) {
 }
 
 
-for (var i = 0; i < directions.length; i++){
+for (var i = 0; i < directions.length; i++) {
     var dir = directions[i];
     dir = dir.trim();
-    
+
     var turn = dir.substring(0, 1);
     var amount = parseInt(dir.substring(1));
-    
+
     direction = getDirection(turn, direction);
-    if (direction === 'West' || direction === 'East') { 
-        x = addXAmount(direction, amount, x); 
-    } else {                  
-       y = addYAmount(direction, amount, y); 
+    if (direction === 'West' || direction === 'East') {
+        x = addXAmount(direction, amount, x);
+    } else {
+        y = addYAmount(direction, amount, y);
     }
 }
 
 var found = false;
-for (var i = 0; i < coords.length; i++){
-    for (var j = i+1; j < coords.length; j++){
-        if (!found && coords[i].x === coords[j].x && coords[i].y === coords[j].y)
-        {
+for (var i = 0; i < coords.length; i++) {
+    for (var j = i + 1; j < coords.length; j++) {
+        if (!found && coords[i].x === coords[j].x && coords[i].y === coords[j].y) {
             found = true;
             console.log("Intersection at (" + coords[i].x + ", " + coords[i].y + ").");
             var answer = Math.abs(coords[i].x) + Math.abs(coords[i].y)
@@ -93,5 +92,5 @@ for (var i = 0; i < coords.length; i++){
 console.log("Number of coords: " + coords.length);
 
 
-//var blocks = Math.abs(x) + Math.abs(y);
-//console.log("Number of blocks: " + blocks);
+var blocks = Math.abs(x) + Math.abs(y);
+console.log("Number of blocks: " + blocks);
