a2ca74db3410f0af269099c55d04fb1788010d3a
James Duvall <Bulldogg6404@users.noreply.github.com>
2016-12-04 11:33:49 -0600

    Update day4_p1.cpp
    There is a logic error in this somewhere. I have no idea what or where, but I do know that the error is "too low" of a result, meaning there are some results not being counted.

--- day4_p1.cpp
+++ day4_p1.cpp
@@ -9,33 +9,58 @@ int main() {
 	// -- if char is letter, increment array index corresponding to each letter
 	// -- if char is number, append to sectorID string
 	// -- if char is left bracket, end traversal
-  // ------------------------------------------------
-  // TODO:
 	// use the array to populate temp string with 5 most frequent characters
 	// compare characters in brackets with temp string
-	// if valid, add atoi sectorID to result
+	// if valid, add stoi sectorID to result
 	// reset temps and repeat until end of stream
 	// output result
 	
-	string name, sectorID, checksum;
-	int result;
-	int alphaMode[255] = {0};
+	string name;
+	int result = 0;
 	cin >> name;
-	for (int i = 0; i < name.size(); i++)
+	
+	while (name[0] != '0')
 	{
-		if (isalpha(name[i]))
-			alphaMode[name[i]]++;
-		else if (isdigit(name[i]))
-			sectorID += name[i];
-		else if (name[i] == '[')
+		string sectorID, checksum, expected;
+		int max, maxIndex;
+		int alphaMode[255] = {0};
+		for (int i = 0; i < name.size(); i++)
 		{
-			i++;
-			while (name[i] != ']')
+			if (isalpha(name[i]))
+				alphaMode[name[i]]++;
+			else if (isdigit(name[i]))
+				sectorID += name[i];
+			else if (name[i] == '[')
 			{
-				checksum += name[i];
 				i++;
+				while (name[i] != ']')
+				{
+					checksum += name[i];
+					i++;
+				}
 			}
 		}
+		// cout << name << " " << sectorID << " " << checksum << " " << alphaMode['p'];
+		// populate a new string with 5 most common letters
+		
+		for(int i = 0; i < 5; i++)
+		{
+			max = alphaMode[0];
+			maxIndex = 0;
+			for(int j = 0; j < name.size() - 7; j++)
+			{
+				if(isalpha(name[j]) && alphaMode[name[j]] > alphaMode[name[maxIndex]])
+					maxIndex = j;
+			}
+			expected += name[maxIndex];
+			alphaMode[name[maxIndex]] = 0;
+		}
+		if (checksum == expected)
+		{
+			//cout << checksum << " " << expected << " " << "true" << " + " << sectorID << endl;
+			result += stoi(sectorID);
+		}
+		cin >> name;
 	}
-	// cout << name << " " << sectorID << " " << checksum << " " << alphaMode['p'];
+	cout << result;
 }
